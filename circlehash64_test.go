41c2a99a502b0611a9feebebf3c530902ee6223a
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2021-10-07 23:04:23 -0500

    Add more tests and remove duplicate tests

--- circlehash64_test.go
+++ circlehash64_test.go
@@ -47,12 +47,12 @@ const (
 	numsAllAAs   = uint64(0xAAAAAAAAAAAAAAAA) // alternating 0 and 1 bit
 	numsAllFFs   = uint64(0xFFFFFFFFFFFFFFFF)
 
-	// https://en.wikipedia.org/wiki/Golden_ratio
 	numsGoldenRatio    = uint64(0x9E3779B97F4A7C15) // https://en.wikipedia.org/wiki/Golden_ratio
 	numsGoldenRatioInv = numsGoldenRatio ^ numsAllFFs
 )
 
 var countCircleHash64f uint64 // count calls to Hash64 (doesn't include calls to HashString64)
+var collisions = make(map[uint64]uint64)
 
 func TestCircleHash64EmptyInputs(t *testing.T) {
 
@@ -115,7 +115,13 @@ func TestCircleHash64UniformBitPatternInputs(t *testing.T) {
 
 func TestCircleHash64NonUniformBitPatternInputs(t *testing.T) {
 
+	// Create 16 KiB of test data from SHA-512 using the simplest
+	// form of SHA-512 feedback loop (nothing-up-my-sleeve).
 	data := nonUniformBytes16KiB()
+
+	// Verify CircleHash64 digests produced from hashing portions of
+	// data using different seed values. Input sizes vary from
+	// 1 to 16384 bytes by varing starting pos and ending pos.
 
 	testCases := []struct {
 		name                     string
@@ -128,6 +134,27 @@ func TestCircleHash64NonUniformBitPatternInputs(t *testing.T) {
 			numsAllZeros,
 			decodeHexOrPanic("8fc041d09087f9f3108ed86422ee6562f4eaf1ad0b1d83ede3f69b14f3798b8a5c80518ea7041f0803882ced33bce34351c5415469957e40ddd806d618742a71"),
 			decodeHexOrPanic("80348e245dec5e09c424411c4dfa9fbbad1cbf68495707e3579bec8c7e7e010f6ff441b6b3987e4da28be39ccd355ae545ca0329284fa39a0d630b941e83355a"),
+		},
+
+		{
+			"seed 55s",
+			numsAll55s,
+			decodeHexOrPanic("714733e2f758328f07556e849cc96b371dba28ed8c6c934f6591a7e4ea90a02dc93bb858639ed62b3aacc26932efe3a47aa4e5b713a8f1c2a5375988fb3fcf05"),
+			decodeHexOrPanic("90ac86e7e8ce973ad402d1db741c7ee320b330ffbbe391c9b20eb9ce07385c66df3f40efd0865ee18894b559cde70f38ec7b01319b2ef2f3f61c64cc8abeca12"),
+		},
+
+		{
+			"seed AAs",
+			numsAllAAs,
+			decodeHexOrPanic("710f68717bf5144e703e10236d9d2cda2b7e8e503aacf4168a088a1be51d3ffe83cf19908e238be15883f6cd25a2c7c71e715173e19fc73f5707ad7626c3b944"),
+			decodeHexOrPanic("042037e4dfaf0072c5048c8043fa6ac1f197f8b3c2140d97ccfe9abd69f7ef6fb6739e0728c40bff272dbd6a0c82f7f04f95a0ca64cdfe73c080b691bd58214e"),
+		},
+
+		{
+			"seed FFs",
+			numsAllAAs,
+			decodeHexOrPanic("710f68717bf5144e703e10236d9d2cda2b7e8e503aacf4168a088a1be51d3ffe83cf19908e238be15883f6cd25a2c7c71e715173e19fc73f5707ad7626c3b944"),
+			decodeHexOrPanic("042037e4dfaf0072c5048c8043fa6ac1f197f8b3c2140d97ccfe9abd69f7ef6fb6739e0728c40bff272dbd6a0c82f7f04f95a0ca64cdfe73c080b691bd58214e"),
 		},
 
 		{
@@ -170,96 +197,15 @@ func TestCircleHash64NonUniformBitPatternInputs(t *testing.T) {
 		})
 	}
 
-}
-
-func TestHash64(t *testing.T) {
-
-	data00 := make([]byte, 256)
-	for i := 0; i < len(data00); i++ {
-		data00[i] = 0
+	/* TODO: (Optional) Repeat tests after inverting the input data.
+	// Create 16 KiB of test data with every bit of data inverted,
+	// to make tests cover all 131072 bits as both 0 and 1.
+	datainv := nonUniformBytes16KiB()
+	for i := 0; i < len(datainv); i++ {
+		datainv[i] ^= uint8(0xFF)
 	}
+	*/
 
-	data55 := make([]byte, 256)
-	for i := 0; i < len(data55); i++ {
-		data55[i] = 0x55
-	}
-
-	data80 := make([]byte, 256)
-	for i := 0; i < len(data80); i++ {
-		data80[i] = 0x80
-	}
-
-	dataAA := make([]byte, 256)
-	for i := 0; i < len(dataAA); i++ {
-		dataAA[i] = 0xaa
-	}
-
-	dataFF := make([]byte, 256)
-	for i := 0; i < len(dataFF); i++ {
-		dataFF[i] = 0xff
-	}
-
-	data00FF := make([]byte, 256)
-	for i := 0; i < len(data00FF); i++ {
-		data00FF[i] = uint8(i)
-	}
-
-	testCases := []struct {
-		name           string
-		seed           uint64
-		data           []byte
-		expectedDigest []byte
-	}{
-		/*
-			// These are SHA384 results for 1,536 different Abseil-compatible digests (256 digests each)
-			{"00", uint64(0x0000000000000000), data00, decodeHexOrPanic("671adad39c36eb415712103bd6120e5400d88cad815edecc0879dc29bb207a1f38ad76acc610cf9888f262ed942a4604")},
-
-			{"55", uint64(0x5555555555555555), data55, decodeHexOrPanic("f27fb9f14f639b6c2642f235e87eb422ef5b01201d07babb9e6ba1e9cd544fbc3f9d83391ce5b414420c5847c93deb88")},
-
-			{"aa", uint64(0xaaaaaaaaaaaaaaaa), dataAA, decodeHexOrPanic("8f979fa390694fc8932a686e9f4d01bcc01d278cff0220b58fbef16754816e362d95f64788f330c2f20e1610d810343b")},
-
-			{"80", uint64(0x8080808080808080), data80, decodeHexOrPanic("626e65cec12993f8b20d487c9a946959ab2f23125822af26a1087083fdfe67ba799f36fc67a615f38ee3b947b6d4f6ae")},
-
-			{"ff", uint64(0xffffffffffffffff), dataFF, decodeHexOrPanic("2786da14eb751de04da7c9838dc98d483a01f7948f97e4234ea1eebb9bbb9a7289afabb0535ae7c004287c562496185b")},
-
-			{"00ff", uint64(0x0123456789ABCDEF), data00FF, decodeHexOrPanic("8219f8c46a86a5f393c8f7fae9b0a11621894591b8d094b7ec727a28acbbb4599da80ccad714fed794af5ed848dea57e")},
-		*/
-
-		// These are SHA384 results for 1,536 CircleHash64f digests (256 digests each)
-		{"00", uint64(0x0000000000000000), data00, decodeHexOrPanic("62d57be0c889c0e50e5b5c225ffcd30e46c5ace73f5fdf97f78eb3893c8fe3bcfb0aa9858d89830172bb7c417256be81")},
-
-		{"55", uint64(0x5555555555555555), data55, decodeHexOrPanic("4c94dd841e6547a20f9c83470f34beea48f7fc9e737714eef736bb343acbf0822d72c8cf4e9c7a6fd3a5aa7b254be5b5")},
-
-		{"aa", uint64(0xaaaaaaaaaaaaaaaa), dataAA, decodeHexOrPanic("704164484daaa9d7236b0bfa836755074496e5f84bb5939a11c2b1d9179f6f083826d4d80134df30445c8e61912b2569")},
-
-		{"80", uint64(0x8080808080808080), data80, decodeHexOrPanic("ab16308e15428ec5807d69646d9c4c9a99fa64b3dd55a73b711192948b5752e019fabfb9a56e61e6941ab43b6cd1aa28")},
-
-		{"ff", uint64(0xffffffffffffffff), dataFF, decodeHexOrPanic("690755c949c92918780ecab6f8f23aacac7ae49ad2eab6c070e1c866ebe43b61668d7c47d790115ba982e6e0381ebd54")},
-
-		{"00-ff", uint64(0x0123456789ABCDEF), data00FF, decodeHexOrPanic("796ddd4981b6e7a9ade46eba606437953e56239619e8e468ad6e24dc4d11be90bbe4fcc23cb5a1c0acd53a55589e202d")},
-	}
-
-	for _, tc := range testCases {
-		t.Run(fmt.Sprintf(tc.name), func(t *testing.T) {
-
-			sha384 := sha512.New384()
-
-			b := make([]byte, 8)
-			for i := 0; i < len(tc.data); i++ {
-				digest := Hash64(tc.data[0:i], tc.seed)
-
-				binary.LittleEndian.PutUint64(b, digest)
-
-				sha384.Write(b)
-			}
-
-			actual := sha384.Sum(nil)
-
-			if !bytes.Equal(tc.expectedDigest, actual) {
-				t.Errorf("sha384(d0, ..., d255) returned %s, want %s", hex.EncodeToString(actual), tc.expectedDigest)
-			}
-		})
-	}
 }
 
 // checksumUniformBitPatternInputs returns SHA-512 checksum of 65536 CircleHash64
@@ -361,6 +307,7 @@ func countedCircleHash64(t *testing.T, data []byte, seed uint64) uint64 {
 		t.Errorf("Hash64() 0x%x != HashString64() 0x%x", digest, digest2)
 	}
 
+	collisions[digest]++
 	countCircleHash64f++
 	return digest
 }
